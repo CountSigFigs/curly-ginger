0cf631d5e034b8f08ce9ecdcca4d32bdd9240356
Brian Holt <bharrisonholt@gmail.com>
2024-12-06 12:01:12 -0500

    updating samesite in cookie

--- src/app/action.ts
+++ src/app/action.ts
@@ -7,7 +7,7 @@ export async function validateSiteEntry(passcode: string){
     if (passcode === process.env.SITE_ENTRY_CODE) {
         const fifteenMinutes = 15 * 60 * 1000;
 
-        cookies().set('entryGranted', 'true', { expires: Date.now() + fifteenMinutes, sameSite:'strict' })
+        cookies().set('entryGranted', 'true', { expires: Date.now() + fifteenMinutes, sameSite:'none' })
         redirect('/dashboard/login');
     }
 }
